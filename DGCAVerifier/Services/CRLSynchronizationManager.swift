2778dd33f047f7f27df25e9e79bb56ded90a611e
Emilio Apuzzo <emilio.apuzzo.ea@outlook.it>
2021-09-16 16:08:33 +0200

    Fix CRLSynchronizationManager

--- DGCAVerifier/Services/CRLSynchronizationManager.swift
+++ DGCAVerifier/Services/CRLSynchronizationManager.swift
@@ -44,7 +44,6 @@ class CRLSynchronizationManager {
         log("initialize")
         self.delegate = delegate
         setTimer() { self.start() }
-        start()
     }
     
     func start() {
